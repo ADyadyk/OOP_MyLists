a66ea1fd22e18106bb586fc398e73b554c2528fd
Dyadyk Alexander <a.dyadyk@gmail.com>
2023-12-28 18:56:17 +0300

    Initial commit

---
+++ src/lesson04/CheckSimpleIterator.java
@@ -0,0 +1,18 @@
+package lesson04;
+
+public class MyInteger {
+    public static void main(String[] args) {
+        Integer[] integers = new Integer[3];
+        integers[0] = 1;
+        integers[1] = 200;
+        integers[2] = 3000;
+
+        // Создадим итератор для массива integers:
+        SimpleIterator<Integer> forIntegers = new SimpleIterator<>(integers);
+
+        // Выведем в консоль все элементы массива integers, использовав его итератор:
+        while (forIntegers.hasNext()){
+            System.out.println(forIntegers.next());
+        }
+    }
+}
